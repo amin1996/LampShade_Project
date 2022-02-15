0df18e42ade85f77f049b5bb1b64eeeb4e445875
Amin Tabibzadeh <32246126+amin1996@users.noreply.github.com>
2022-02-15 08:57:24 +0330

    services registered

--- ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -1,20 +1,11 @@
-﻿using ShopManagement.Application.Contracts.ProductCategory;
-using System;
+﻿using _0_FrameWork.Domain;
+using ShopManagement.Application.Contracts.ProductCategory;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-
+ 
 namespace ShopManagement.Domain.ProductCategoryAgg
 {
-    public interface IProductCategoryRepository
-    {
-        void Create(ProductCategory entity);
-        ProductCategory Get (long id);
-        List<ProductCategory> GetAll();
-        bool Exists(Expression<Func<ProductCategory,bool>>  expression);
-        void SaveChanges();
+    public interface IProductCategoryRepository:IRepository<long,ProductCategory>
+    {       
         EditProductCategory GetDetails(long id);
         List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
     }
--- ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
+++ ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using ShopManagement.Application.Contracts.ProductCategory;
+﻿using _0_FrameWork.Infrastructure;
+using ShopManagement.Application.Contracts.ProductCategory;
 using ShopManagement.Domain.ProductCategoryAgg;
 using System;
 using System.Collections.Generic;
@@ -9,33 +10,13 @@ using System.Threading.Tasks;
 
 namespace ShopManagement.Infrastructure.EFCore.Repository
 {
-    public class ProductCategoryRepository : IProductCategoryRepository
+    public class ProductCategoryRepository :RepositoryBase<long,ProductCategory> ,IProductCategoryRepository
     {
         private readonly ShopContext _context;
 
-        public ProductCategoryRepository(ShopContext context)
+        public ProductCategoryRepository(ShopContext context) : base(context)
         {
             _context = context;
-        }
-
-        public void Create(ProductCategory entity)
-        {
-            _context.ProductCategories.Add(entity);
-        }
-
-        public bool Exists(Expression<Func<ProductCategory, bool>> expression)
-        {
-            return _context.ProductCategories.Any(expression);
-        }
-
-        public ProductCategory Get(long id)
-        {
-            return _context.ProductCategories.Find(id);
-        }
-
-        public List<ProductCategory> GetAll()
-        {
-            return _context.ProductCategories.ToList();
         }
 
         public EditProductCategory GetDetails(long id)
@@ -53,12 +34,6 @@ namespace ShopManagement.Infrastructure.EFCore.Repository
                 Slug = x.Slug,
             }).FirstOrDefault(x => x.Id == id);
         }
-
-        public void SaveChanges()
-        {
-            _context.SaveChanges();
-        }
-
         public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
         {
             var query = _context.ProductCategories.Select(x => new ProductCategoryViewModel()
